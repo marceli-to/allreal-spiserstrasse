2eb92824baf708b7f30ac7ee2042aa0da313ae53
Marcel Stadelmann <m@marceli.to>
2023-03-03 19:10:48 +0100

    wip

--- resources/js/web/modules/iso.js
+++ resources/js/web/modules/iso.js
@@ -4,7 +4,11 @@
     isoItem: '[data-iso-item]',
     listItem: '[data-list-item]',
     thumbItem: '[data-thumb-item]',
+    isoView: '[data-iso-view]',
+    btnRotate: '[data-btn-rotate]',
   };
+
+  let currentView = 2;
 
   const init = () => {
     const isoItems = [].slice.call(
@@ -12,11 +16,41 @@
     );
     
     // Add event listener for mouse over
-    isoItems.forEach(function (item) {
-      item.addEventListener("mouseover", function(){
-        console.log(item);
+    // isoItems.forEach(function (item) {
+    //   item.addEventListener("mouseover", function(){
+    //   }, false);
+    // });
+
+    // Add click event on rotate button if it exists
+    const btnRotate = document.querySelector(selectors.btnRotate);
+    if (btnRotate) {
+      btnRotate.addEventListener("click", function(){
+        rotate();
       }, false);
+    }
+  };
+
+  const rotate = () => {
+    hideAllViews();
+    showCurrentView();
+  };
+
+  const hideAllViews = () => {
+    const isoViews = [].slice.call(
+      document.querySelectorAll(selectors.isoView)
+    );
+    isoViews.forEach(function (view) {
+      view.classList.add('hidden');
     });
+  };
+
+  const showCurrentView = () => {
+    currentView = currentView < 4 ? currentView + 1 : 1;
+    const isoView = document.querySelector('[data-iso-view="' + currentView + '"]');
+    isoView.classList.remove('hidden');
+    const btnRotate = document.querySelector(selectors.btnRotate);
+    // Update inner html of rotate button
+    btnRotate.innerHTML = 'Rotate (' + currentView + ')';
   };
 
   init();
--- resources/views/web/pages/apartments.blade.php
+++ resources/views/web/pages/apartments.blade.php
@@ -5,6 +5,9 @@
 <div class="grid grid-cols-16 gap-16 mb-44 md:sticky md:top-0 z-10 bg-white">
   <div class="col-span-10 lg:col-start-2 lg:col-span-8">
     @include('web.pages.partials.apartments.isometrie')
+
+    <a href="javascript:;" data-btn-rotate>Rotate</a>
+
   </div>
   <div class="col-start-12 col-span-4 lg:col-start-11 lg:col-span-3">
     @include('web.pages.partials.apartments.filter')
