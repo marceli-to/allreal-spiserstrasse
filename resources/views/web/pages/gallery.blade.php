19d082f5c99a6710be1f7dcc92d72f275e718ee6
Marcel Stadelmann <m@marceli.to>
2023-03-10 11:23:19 +0100

    wip

--- resources/views/web/pages/gallery.blade.php
+++ resources/views/web/pages/gallery.blade.php
@@ -5,7 +5,6 @@
 <x-swiper>
   <x-swiper.slide :image="'Allreal_Spiserstrasse_Aussen_Innenhof'" />
   <x-swiper.slide :image="'Allreal_Spiserstrasse_Aussen_Terrasse'" />
-  <x-swiper.slide :image="'Allreal_Spiserstrasse_Aussen_Vogel-2'" />
   <x-swiper.slide :image="'Allreal_Spiserstrasse_Aussen_Vogel'" />
   <x-swiper.slide :image="'Allreal_Spiserstrasse_Innen_Bad-4-5'" />
   <x-swiper.slide :image="'Allreal_Spiserstrasse_Innen_Kueche-4-5'" />
