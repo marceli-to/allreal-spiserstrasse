80cd39f027cdc8cc5793c79021d326fc665ce63c
Marcel Stadelmann <m@marceli.to>
2023-03-29 13:53:36 +0200

    wip

--- resources/views/components/news/index.blade.php
+++ resources/views/components/news/index.blade.php
@@ -2,7 +2,7 @@
   <h2 class="uppercase text-blue text-lg mt-36 md:mt-0 md:bg-white md:py-10 md:px-20">Aktuell</h2>
   <section class="mt-32 mb-48 md:mt-0 py-8 md:py-12 md:px-20 md:mb-0 border-t border-t-anthrazit md:border-0 md:bg-white">
     <article class="">
-      <div>20.02.2023</div>
+      <div>20. März 2023</div>
       <h3 class="mt-2 font-regular">Baubeginn an der Spiserstrasse</h3>
       <p>Im Anschluss an die Vorbereitungsarbeiten begann am 20. März 2023 der Rückbau <a href="{{ route('page.news', ['slug' => 'baubeginn-an-der-spieserstrasse']) }}" title="Baubeginn an der Spiserstrasse">[...]</a></p>
     </article>
--- resources/views/web/pages/home.blade.php
+++ resources/views/web/pages/home.blade.php
@@ -8,7 +8,7 @@
 <section class="md:mt-48 flex flex-col md:grid md:grid-cols-16">
   <div class="order-2 md:order-1 md:col-span-10 md:col-start-1">
     <p class="font-light text-lg break-words hyphen-auto">
-      An der Spiserstrasse in Zürich Albisrieden entsteht bis Anfang 2025 die Wohnüberbauung Spiserstrasse mit 57 Eigentumswohnungen, sechs Townhouses sowie sechs Ateliers. Die Überbauung wird im Minergie-Eco-Standard realisiert.
+      An der Spiserstrasse in Zürich Albisrieden entsteht bis Mitte 2025 die Wohnüberbauung Spiserstrasse mit 57 Eigentumswohnungen, sechs Townhouses sowie sechs Ateliers. Die Überbauung wird im Minergie-Eco-Standard realisiert.
     </p>
   </div>
   <x-news />
